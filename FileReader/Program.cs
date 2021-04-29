b2a816067bed3954a12d422b311a7e28a1f4284c
Jeroen <decleerj@gmail.com>
2021-04-29 21:06:27 +0200

    Add project files.

---
+++ FileReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace FileReader
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string bestand = "FileReader.files.tekst.txt";
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(bestand)) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    string result = reader.ReadToEnd();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
