b7f01e3fceb46f2e55af0ddea19482bc9d3d87e7
Jeroen <decleerj@gmail.com>
2021-05-06 20:55:05 +0200

    update3.0

--- FileReader/Program.cs
+++ FileReader/Program.cs
@@ -9,11 +9,13 @@ namespace FileReader
     class Program
     {
         static void Main(string[] args) {
+            bool encrypted = false;
+            bool adminrole = false;
+
             Dictionary<int, string> bestanden = new Dictionary<int, string>();
             bestanden.Add(0, "Exit");
             bestanden.Add(1, "tekst.txt");
             bestanden.Add(2, "tekst.xml");
-            bestanden.Add(3, "tekstencrypted.txt");
 
             int keuzegetal = -1;
             int aantalbestanden = bestanden.Count;
@@ -24,11 +26,21 @@ namespace FileReader
 
                 string keuze = Console.ReadLine();
 
+                Console.WriteLine("Wil je het bestand encrypteren? druk (j)");
+                string encryptedinput = Console.ReadLine();
+
+                if (encryptedinput.ToLower() == "j") {
+                    encrypted = true;
+                }
+                else {
+                    encrypted = false;
+                }
+
                 bool success = Int32.TryParse(keuze, out keuzegetal);
                 if (success && keuzegetal >= 0 && keuzegetal < aantalbestanden) {
                     string folder = "FileReader.files.";
                     string bestand = folder + bestanden[keuzegetal];
-                    Console.WriteLine(ReadFile(bestand, keuzegetal));
+                    Console.WriteLine(ReadFile(bestand, keuzegetal, encrypted));
                 }
                 else {
                     Console.WriteLine("Gelieve een geldig getal op te geven!");
@@ -36,14 +48,13 @@ namespace FileReader
                 }
             }
         }
-
-        private static string ReadFile(string bestand, int keuzegetal) {
+        private static string ReadFile(string bestand, int keuzegetal, bool encrypted) {
             try {
                 using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(bestand)) {
                     using (StreamReader reader = new StreamReader(stream)) {
                         string result = reader.ReadToEnd();
 
-                        if(keuzegetal == 3) {
+                        if(encrypted == true) {
                             result = Reverse(result);
                         }
                         return result;
